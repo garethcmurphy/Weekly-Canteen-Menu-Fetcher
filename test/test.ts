364dced4b83aa65e4dc46fbc4b18ca6915f99d5f
garethcmurphy <garethcmurphy@gmail.com>
2017-12-08 22:38:49 +0100

    cleanup

--- test/test.ts
+++ test/test.ts
@@ -4,13 +4,12 @@ import {expect} from 'chai'
 
 import {Scraper} from "../src/index";
 
-import {} from 'jasmine';
-
 
 describe('Access Token', () => {
     it('checks token is string', () => {
         let access = new Scraper();
-        let token = access.access_token;
+        let token = access.url;
+        access.set_url();
         expect(token).to.be.a('string');
         expect(token).to.have.lengthOf(64);
     });
